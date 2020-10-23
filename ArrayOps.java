ae9d8d91ba1855a5a161da1bf93e958df2747c0d
thomasyu21 <thomasyu085@gmail.com>
2020-10-23 16:45:06 -0400

    Created sum() for a 2D array and tested it using 2D array A

--- ArrayOps.java
+++ ArrayOps.java
@@ -34,4 +34,12 @@ public class ArrayOps {
     return withLargest;
   }
 
+  public static int sum(int[][] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum+=sum(arr[i]);
+    }
+    return sum;
+  }
+
 }
