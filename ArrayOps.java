51fabf49782f0c2be6f095c7c8fbb62f7e57e8f4
thomasyu21 <thomasyu085@gmail.com>
2020-10-23 16:26:49 -0400

    Added arrToString and arrayToDeepString to test future methods

--- ArrayOps.java
+++ ArrayOps.java
@@ -18,4 +18,25 @@ public class ArrayOps {
     return largestNum;
   }
 
+  public static String arrToString(int[] arr){
+    if (arr.length == 0) {
+      return "{}";
+    }
+    String converted = "{" + arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      converted+= (", " + arr[i]);
+    }
+    converted+= "}";
+    return converted;
+  }
+
+  public static String arrayDeepToString(int[][] arr) {
+    String newStr = "{" + arrToString(arr[0]);
+    for (int i = 1; i < arr.length; i++){
+      newStr+=(", " + arrToString(arr[i]));
+    }
+    newStr+="}";
+    return newStr;
+  }
+
 }
